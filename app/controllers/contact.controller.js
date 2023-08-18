f098d539c6a94c48df7dda4bbdfbd47520c572b2
Paramjeet Rawat <rawatparam46@gmail.com>
2023-08-18 12:38:08 +0530

    api

--- app/controllers/contact.controller.js
+++ app/controllers/contact.controller.js
@@ -1,3 +1,76 @@
 const db = require("../models");
 const Contact = db.contact;
 const Op = db.Sequelize.Op;
+
+// Create contact
+const ContactController = {
+add_contact : async (req, res) => {
+    try {
+        const { fullname, address, contactno, zip, email, created_by } = req.body;
+        const contact = await Contact.create({
+            fullname,
+            address,
+            contactno,
+            zip,
+            email,
+            created_by,
+            created: new Date(),
+            updated: new Date(),
+        });
+        res.status(201).json({ message: 'Contact added successfully' });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'Internal server error' });
+    }
+}
+};
+
+// Contact details with user details
+app.get('/contact_details/:contactId', async (req, res) => {
+    try {
+        const contactId = req.params.contactId;
+        const contact = await Contact.findByPk(contactId, { include: User });
+        if (!contact) {
+            return res.status(404).json({ message: 'Contact not found' });
+        }
+        res.json({
+            contact: {
+                fullname: contact.fullname,
+                address: contact.address,
+                contactno: contact.contactno,
+                zip: contact.zip,
+                email: contact.email,
+            },
+            created_by: contact.user,
+        });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'Internal server error' });
+    }
+});
+
+// User details with all contacts
+app.get('/user_details/:userId', async (req, res) => {
+    try {
+        const userId = req.params.userId;
+        const user = await User.findByPk(userId, { include: Contact });
+        if (!user) {
+            return res.status(404).json({ message: 'User not found' });
+        }
+        const contacts = user.contacts.map(contact => ({
+            fullname: contact.fullname,
+            address: contact.address,
+            contactno: contact.contactno,
+            zip: contact.zip,
+            email: contact.email,
+        }));
+        res.json({ user: user.username, contacts });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'Internal server error' });
+    }
+});
+
+module.exports = ContactController;
+
+
--- app/controllers/user.controller.js
+++ app/controllers/user.controller.js
@@ -1,6 +1,8 @@
 const db = require("../models");
+const Users = require("../models/user.model");
 const User = db.user;
 const Op = db.Sequelize.Op;
+
 
 
 // const UserController = {
@@ -14,96 +16,29 @@ const Op = db.Sequelize.Op;
 //     }
 //   },
 // }
+
+// register user
 const UserController = {
-register: async (req, res) => {
-  try {
-    // Extract user data from request body
-    const { firstname, lastname, username, password, email, phone } = req.body;
-
-    // Create a new user
-    const newUser = await User.create({
-      firstname,
-      lastname,
-      username,
-      password,
-      email,
-      phone,
-    });
-
-    res.status(201).json(newUser);
-  } catch (error) {
-    res.status(500).json({ error: error.message });
+  register: async (req, res) => {
+    const data = {
+      "firstname": "om",
+      "lastname": "ram",
+      "username": "om1",
+      "password": "234",
+      "email": "qwe",
+      "phone": "12323"
+    }
+    console.log("data1", data);
+    try {
+      const book = await Users.create(data);
+      console.log("book", book);
+      res.send(book);
+    } catch (err) {
+      res.send(err);
+    }
   }
-}
+
 }
 
 module.exports = UserController;
 
-
-// API to register a new user
-// app.post('/register', async (req, res) => {
-//   try {
-//     const hashedPassword = await bcrypt.hash(req.body.password, 10);
-
-//     const user = await User.create({
-//       firstname: req.body.firstname,
-//       lastname: req.body.lastname,
-//       username: req.body.username,
-//       password: hashedPassword,
-//       email: req.body.email,
-//       phone: req.body.phone,
-//     });
-
-//     res.status(201).json(user);
-//   } catch (error) {
-//     res.status(500).json({ error: 'Registration failed' });
-//   }
-// });
-
-// API to get all users
-// app.get('/users', async (req, res) => {
-//   try {
-//     const users = await User.findAll();
-//     res.status(200).json(users);
-//   } catch (error) {
-//     res.status(500).json({ error: 'Failed to fetch users' });
-//   }
-// });
-
-// Add more APIs (login, logout, forgot password, reset password, change password)
-
-// API to add a new contact
-// app.post('/contacts', async (req, res) => {
-//   try {
-//     const contact = await Contact.create({
-//       fullname: req.body.fullname,
-//       address: req.body.address,
-//       contactno: req.body.contactno,
-//       zip: req.body.zip,
-//       email: req.body.email,
-//       created_by: req.body.created_by,
-//     });
-
-//     res.status(201).json(contact);
-//   } catch (error) {
-//     res.status(500).json({ error: 'Failed to add contact' });
-//   }
-// });
-
-// API to get user details and their contacts by user ID
-// app.get('/user/:id', async (req, res) => {
-//   try {
-//     const user = await User.findByPk(req.params.id, {
-//       include: Contact,
-//     });
-
-//     if (!user) {
-//       res.status(404).json({ error: 'User not found' });
-//       return;
-//     }
-
-//     res.status(200).json(user);
-//   } catch (error) {
-//     res.status(500).json({ error: 'Failed to fetch user details' });
-//   }
-// });
